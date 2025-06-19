9b8fe78c62874ade4fbfebdca414b67b9b0a03c2
Madison McGill <madisonmcgill@Mac.lan>
2025-06-19 17:42:03 -0400

    Fix PORT line with clean quotes

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ def generate():
 
 if __name__ == '__main__':
     import os
-    port = int(os.environ.get(\"PORT\", 5000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
 
 # trigger redeploy
